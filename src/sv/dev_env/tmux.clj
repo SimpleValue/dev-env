2fc0fb59612fc1e221e33a96e9e858cc6d21d06a
Max Weber <weber.maximilian@gmail.com>
2021-07-11 18:20:12 +0200

    Fix: passing shell fn as argument

--- src/sv/dev_env/tmux.clj
+++ src/sv/dev_env/tmux.clj
@@ -1,49 +1,42 @@
-(ns sv.dev-env.tmux
-  (:require [babashka.process :as process]))
-
-(defn- shell
-  [cmd]
-  @(process/process
-    ["bash" "-c" cmd]
-    {:out :inherit
-     :err :inherit}))
+(ns sv.dev-env.tmux)
 
 (defn new-window!
-  [n name]
+  [shell n name]
   (shell (str "tmux new-window -t dev:"
               n
               " -n '" name "'")))
 
 (defn select-window!
-  [n]
+  [shell n]
   (shell (str "tmux select-window -t dev:"
               n)))
 
 (defn execute!
-  [cmd]
+  [shell cmd]
   (shell (str "tmux send-keys -t dev '"
               cmd "' enter")))
 
 (defn start!
-  [{:keys [name n cmd]}]
-  (new-window! n name)
-  (select-window! n)
-  (execute! cmd))
+  [shell {:keys [name n cmd]}]
+  (new-window! shell n name)
+  (select-window! shell n)
+  (execute! shell cmd))
 
 (defn start-all!
-  [tasks]
+  [shell tasks]
   (doseq [[n task] (map vector
                         (range)
                         tasks)]
-    (start! {:n (inc n)
+    (start! shell
+            {:n (inc n)
              :name task
              :cmd (str "bb "
                        task)})))
 
 (defn new-session!
-  []
+  [shell]
   (shell "tmux -2 new-session -d -s dev"))
 
 (defn attach-session!
-  []
+  [shell]
   (shell "tmux -2 attach-session -t dev"))
