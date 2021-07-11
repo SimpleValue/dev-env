95a450ea05bc85dd69c51292878bae0eb86ffaf8
Max Weber <weber.maximilian@gmail.com>
2021-07-11 18:40:26 +0200

    Added `sv.dev-env.start`

---
+++ src/sv/dev_env/start.clj
@@ -0,0 +1,32 @@
+(ns sv.dev-env.start
+  (:require [clojure.java.io :as io]))
+
+(defn start
+  [{:keys [image ports env-vars cmd container-name]}]
+  (concat
+   ["docker" "run"
+    "-it"
+    "-v" (str (.getCanonicalPath (io/file "dev-home"))
+              ":/home/dev")
+    "-v" (str (.getCanonicalPath (io/file "."))
+              ":/home/dev/app")
+    "-v" (str (System/getProperty "user.home")
+              "/.ssh"
+              ":/home/dev/.ssh/")
+    "--name" (or container-name
+                 "dev")
+    "--rm"
+    "--ipc=host"
+    "--privileged"
+    ]
+   (mapcat
+    (fn [{:keys [host-port container-port]}]
+      ["-p" (str host-port ":" container-port)])
+    ports)
+   (mapcat
+    (fn [[k v]]
+      ["-e" (str k "=" v)])
+    env-vars)
+   [image]
+   (or cmd
+       ["bb" "dev-tmux"])))
